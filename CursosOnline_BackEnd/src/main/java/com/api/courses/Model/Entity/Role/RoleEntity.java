093962cffa9bf9931ed367096dfffef5ad251e05
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-06-07 20:09:52 -0500

    Creacion de gestion de roles

--- CursosOnline_BackEnd/src/main/java/com/api/courses/Model/Entity/Role/RoleEntity.java
+++ CursosOnline_BackEnd/src/main/java/com/api/courses/Model/Entity/Role/RoleEntity.java
@@ -1,4 +1,38 @@
 package com.api.courses.Model.Entity.Role;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "Rol")
 public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol")
+    private Long id;
+
+    @NotEmpty
+    @Column(name = "nombre_rol")
+    private String nombre;
+
+    public RoleEntity() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
