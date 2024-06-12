fb57d67fa246abdf985cea5da8c93ec0f754aaa1
Luis Espinosa <lxespinosa@espe.edu.ec>
2024-06-12 07:58:21 -0500

    boton registro

--- CursosOnline_FrontEnd/src/app/login/login.module.ts
+++ CursosOnline_FrontEnd/src/app/login/login.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
 import { RegistroComponent } from './registro/registro.component';
 
 
@@ -11,7 +20,15 @@ import { RegistroComponent } from './registro/registro.component';
     RegistroComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  providers: [],
 })
-export class LoginModule { }
+export class AppModule { }
