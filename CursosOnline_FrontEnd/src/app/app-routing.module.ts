733e8c3f0fe73e97af9f50d381090cc3327e1b55
Luis Espinosa <lxespinosa@espe.edu.ec>
2024-06-12 19:31:26 -0500

    crear admin module & admin-layout component

--- CursosOnline_FrontEnd/src/app/app-routing.module.ts
+++ CursosOnline_FrontEnd/src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
   },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
 ];
 
 @NgModule({
