12b13a45ec7ab3a67472502449953875f6367099
Luis Espinosa <lxespinosa@espe.edu.ec>
2024-06-13 09:42:28 -0500

    layout-admin

--- CursosOnline_FrontEnd/src/app/admin/admin-layout/admin-layout.component.ts
+++ CursosOnline_FrontEnd/src/app/admin/admin-layout/admin-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-admin-layout',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-layout.component.scss']
 })
 export class AdminLayoutComponent implements OnInit {
+  userData: { id: string, nombre: string, apellido: string, rol: string } | null = null;
 
-  constructor() { }
-
+  constructor(
+    public dialog: MatDialog,
+  ) {}
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+
 
 }
