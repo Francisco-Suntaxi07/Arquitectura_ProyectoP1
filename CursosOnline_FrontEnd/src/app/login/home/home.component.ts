751ad8ce91e1d22d9731f66015b181d74eed1433
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-06-13 17:45:49 -0500

    Fix home component

--- CursosOnline_FrontEnd/src/app/login/home/home.component.ts
+++ CursosOnline_FrontEnd/src/app/login/home/home.component.ts
@@ -3,6 +3,7 @@ import { SignupComponent } from '../signup/signup.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UserService } from 'src/app/service/user.service';
 import { CreatorService } from 'src/app/service/creator.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,16 +15,21 @@ export class HomeComponent {
   constructor(
     public dialog: MatDialog,
     private userService: UserService,
-    private creatorService: CreatorService
+    private creatorService: CreatorService,
+    private router: Router,
   ) { }
+
+  loadSession(): void {
+    this.router.navigate(['/admin']);
+  }
 
   openSignIn(): void {
     const dialogRef = this.dialog.open(SignupComponent);
     dialogRef.afterClosed().subscribe(result => {
-      this.userService.findAll().subscribe( dataListUser => {
+      this.userService.findAll().subscribe(dataListUser => {
         console.log(dataListUser);
       });
-      this.creatorService.findAll().subscribe( dataListCreator => {
+      this.creatorService.findAll().subscribe(dataListCreator => {
         console.log(dataListCreator);
       });
     });
