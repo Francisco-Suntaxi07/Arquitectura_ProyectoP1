d2eed54745dff58eddbea333716f02a795849249
Luis Espinosa <lxespinosa@espe.edu.ec>
2024-06-14 01:23:14 -0500

    pantallas principales

--- CursosOnline_FrontEnd/src/app/consumer/dashboard-consumer/dashboard-consumer.component.ts
+++ CursosOnline_FrontEnd/src/app/consumer/dashboard-consumer/dashboard-consumer.component.ts
@@ -5,11 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard-consumer.component.html',
   styleUrls: ['./dashboard-consumer.component.scss']
 })
-export class DashboardConsumerComponent implements OnInit {
+export class DashboardConsumerComponent {
+  courses = [
+    {
+      title: 'Web design & development courses for beginners',
+      instructor: 'John Doe',
+      rating: 4.5
+    },
+    // Agrega más cursos aquí
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  stats = [
+    { value: 23, label: 'MATERIAS', subLabel: 'DISPONIBLES', bgClass: 'bg-success' },
+    { value: 120, label: 'CURSOS', subLabel: 'ONLINE', bgClass: 'bg-primary' },
+    { value: 42, label: 'INSTRUCTORES', subLabel: 'REGISTRADOS', bgClass: 'bg-secondary' },
+    { value: 6000, label: 'ESTUDIANTES', subLabel: 'FELICES', bgClass: 'bg-warning' },
+  ];
 }
