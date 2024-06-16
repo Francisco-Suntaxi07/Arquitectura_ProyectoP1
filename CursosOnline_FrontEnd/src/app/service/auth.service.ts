fef8aa663725b6bc14f4e98a1c426b8a02834f55
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-06-16 00:11:44 -0500

    Creacion del servicio para sesiones auth

---
+++ CursosOnline_FrontEnd/src/app/service/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { UserModel } from '../model/userModel';
+import { CreatorModel } from '../model/creatorModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private currentUserKey = 'currentUser';
+
+  constructor() { }
+
+  // Método para iniciar sesión
+  login(user: UserModel | CreatorModel): void {
+    localStorage.setItem(this.currentUserKey, JSON.stringify(user));
+  }
+
+  // Método para obtener el usuario actual
+  getCurrentUser(): UserModel | CreatorModel | null {
+    const userString = localStorage.getItem(this.currentUserKey);
+    if (userString) {
+      return JSON.parse(userString);
+    } else {
+      return null;
+    }
+  }
+
+  // Método para cerrar sesión
+  logout(): void {
+    localStorage.removeItem(this.currentUserKey);
+  }
+
+  // Método para comprobar si el usuario está autenticado
+  isAuthenticated(): boolean {
+    return this.getCurrentUser() !== null;
+  }
+
+}
--- CursosOnline_FrontEnd/src/app/shared/header/header.component.ts
+++ CursosOnline_FrontEnd/src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private autService:AuthService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  logOut():void {
+    this.autService.logout();
   }
 
 }
